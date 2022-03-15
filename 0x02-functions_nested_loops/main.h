98f583d38b8a299310b57f07d0ec4a030c1c977b
Kcee42 <chilakakelechi85@gmail.com>
2022-03-15 15:57:59 -0700

    Main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+/**
+ * main - collating of functions
+ * Return: The values
+ */
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+#endif
