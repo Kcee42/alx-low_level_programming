7cc115c3e7657d21928c2e837be24ec9ce78b753
Kcee42 <chilakakelechi85@gmail.com>
2022-03-16 18:09:54 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet _ print all alphabets
+ */
+void print_alphabet(void)
+{
+	char 1 = 'a';
+
+	while (1 <= 'z')
+	{
+		_putchar(1);
+		1++;
+	}
+	_putchar('\n');
+}
+
